750fa415d12a3b2f3d0971e0534daab50bec6705
Tim <Tim280696@hotmail.com>
2023-01-24 17:08:37 +0100

    add links

--- src/Components/Thumbnail/Thumbnail.tsx
+++ src/Components/Thumbnail/Thumbnail.tsx
@@ -1,13 +1,15 @@
 import "./Thumbnail.scss";
+import { Link } from "react-router-dom";
 
 type Props = {
   title: string;
+  url: string;
 };
 
-export default function Thumbnail({ title }: Props) {
+export default function Thumbnail({ title, url }: Props) {
   return (
-    <div className="thumbnail">
+    <Link className="thumbnail" to={url}>
       <h2 className="thumbnail-title">{title}</h2>
-    </div>
+    </Link>
   );
 }
