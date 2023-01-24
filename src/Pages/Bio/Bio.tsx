52b47b45394285446e34c700975299bf79a9b991
Tim <Tim280696@hotmail.com>
2023-01-24 17:08:58 +0100

    create component

---
+++ src/Pages/Bio/Bio.tsx
@@ -0,0 +1,12 @@
+import Banner from "../../Components/Banner/Banner";
+import DropDownMenu from "../../Components/DropDownMenu/DropDownMenu";
+import "./Bio.scss";
+
+export default function Bio() {
+  return (
+    <>
+      <Banner />
+      <DropDownMenu />
+    </>
+  );
+}
---
+++ src/Pages/Contact/Contact.tsx
@@ -0,0 +1,12 @@
+import Banner from "../../Components/Banner/Banner";
+import DropDownMenu from "../../Components/DropDownMenu/DropDownMenu";
+import "./Contact.scss";
+
+export default function Contact() {
+  return (
+    <>
+      <Banner />
+      <DropDownMenu />
+    </>
+  );
+}
