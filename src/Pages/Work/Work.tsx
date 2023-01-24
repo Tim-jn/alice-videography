83c05a9a70645969e42df7e6975cb4111199d00c
Tim <Tim280696@hotmail.com>
2023-01-24 16:47:02 +0100

    create work page

---
+++ src/Pages/Work/Work.tsx
@@ -0,0 +1,12 @@
+import Banner from "../../Components/Banner/Banner";
+import DropDownMenu from "../../Components/DropDownMenu/DropDownMenu";
+import "./Work.scss";
+
+export default function Work() {
+  return (
+    <>
+      <Banner />
+      <DropDownMenu />
+    </>
+  );
+}
