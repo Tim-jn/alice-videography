c2fadcc5bb98ffca435737995fa40a7f9e8ef5bb
Tim <Tim280696@hotmail.com>
2023-01-24 20:40:58 +0100

    add new components

--- src/Pages/Work/Work.tsx
+++ src/Pages/Work/Work.tsx
@@ -1,19 +1,23 @@
+import BackToTop from "../../Components/BackToTop/BackToTop";
 import Banner from "../../Components/Banner/Banner";
 import DropDownMenu from "../../Components/DropDownMenu/DropDownMenu";
+import Footer from "../../Components/Footer/Footer";
 import VideoContent from "../../Components/VideoContent/VideoContent";
 import { work } from "../../data/work";
 import "./Work.scss";
 
 export default function Work() {
   return (
-    <>
+    <main className="work">
       <Banner />
       <DropDownMenu />
+      <BackToTop />
       <section className="work-content">
         {work.map(({ id, title, url }) => {
           return <VideoContent key={id} title={title} url={url} />;
         })}
       </section>
-    </>
+      <Footer />
+    </main>
   );
 }
