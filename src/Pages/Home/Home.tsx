dac179589423d76cebc8cd55c0916a36f1c71875
Tim <Tim280696@hotmail.com>
2023-01-24 20:40:51 +0100

    add new components

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,17 +3,21 @@ import Thumbnail from "../../Components/Thumbnail/Thumbnail";
 import Banner from "../../Components/Banner/Banner";
 import DropDownMenu from "../../Components/DropDownMenu/DropDownMenu";
 import { pages } from "../../data/pages";
+import Footer from "../../Components/Footer/Footer";
+import BackToTop from "../../Components/BackToTop/BackToTop";
 
 export default function Home() {
   return (
-    <>
+    <main className="home">
       <Banner />
       <DropDownMenu />
+      <BackToTop />
       <section className="thumbnail-gallery">
         {pages.map(({ id, title, url }) => {
           return <Thumbnail key={id} title={title} url={url} />;
         })}
       </section>
-    </>
+      <Footer />
+    </main>
   );
 }
