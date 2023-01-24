63a8a698f9bbd94cebb9e6c8af9bc834c59cb088
Tim <Tim280696@hotmail.com>
2023-01-24 17:08:20 +0100

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Work from "./Pages/Work/Work";
+import Bio from "./Pages/Bio/Bio";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
+        <Route path="/bio" element={<Bio />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
