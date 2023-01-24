1e4deed4ecb75b5ea0ab338fd42d2a3545c2ea1a
Tim <Tim280696@hotmail.com>
2023-01-24 20:46:47 +0100

    add redirect to home on banner

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,13 +1,16 @@
 import "./Banner.scss";
+import { Link } from "react-router-dom";
 
 export default function Banner() {
   return (
     <header className="Banner">
-      <h1 className="banner-title">
-        alice j.
-        <br />
-        videography
-      </h1>
+      <Link to="/">
+        <h1 className="banner-title">
+          alice j.
+          <br />
+          videography
+        </h1>
+      </Link>
     </header>
   );
 }
