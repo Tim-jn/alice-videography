ca28b34efa0c816cf958263459ce4898d06fbe3a
Tim <Tim280696@hotmail.com>
2023-01-24 15:30:13 +0100

    add home content

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Banner from "./Components/Banner/Banner";
 import DropDownMenu from "./Components/DropDownMenu/DropDownMenu";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <main className="App">
       <Banner />
       <DropDownMenu />
+      <Home />
     </main>
   );
 }
