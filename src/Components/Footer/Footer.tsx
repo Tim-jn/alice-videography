57d8931d6c8ec44fbe5871af20ab755963aa2749
Tim <Tim280696@hotmail.com>
2023-01-24 20:40:21 +0100

    create footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <footer>
+      copyright © 2023 - designed and created by
+      <a href="https://timjeanmart.be" target="_blank" rel="noreferrer">
+        tim jeanmart
+      </a>
+    </footer>
+  );
+}
