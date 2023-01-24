490c73a49c950c1a3f65de6e367bb9ba5de79bae
Tim <Tim280696@hotmail.com>
2023-01-24 14:27:10 +0100

    init project

---
+++ src/Components/Banner/Banner.tsx
@@ -0,0 +1,13 @@
+import "./Banner.scss";
+
+export default function Banner() {
+  return (
+    <div className="Banner">
+      <h1 className="banner-title">
+        Alice J.
+        <br />
+        Vidéography
+      </h1>
+    </div>
+  );
+}
---
+++ src/Components/DropDownMenu/DropDownMenu.tsx
@@ -0,0 +1,38 @@
+import "./DropDownMenu.scss";
+import { useState } from "react";
+
+export default function DropDownMenu() {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleSidebar = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <div className={!toggle ? "sidebar" : "sidebar sidebar-active"}>
+      <div className="dropdown-button" onClick={toggleSidebar}></div>
+      <div className="close-button" onClick={toggleSidebar}></div>
+      {toggle ? (
+        <ul className="nav-list">
+          <li>
+            <a href="#">
+              <span className="link-name">Work</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="link-name">Bio</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span className="link-name">Contact</span>
+            </a>
+          </li>
+        </ul>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
