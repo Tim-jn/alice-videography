98cee0fc740e1508126fac506fe24f98518851e3
Tim <Tim280696@hotmail.com>
2023-02-01 15:53:06 +0100

    fix integration error

--- src/data/work.js
+++ src/data/work.js
@@ -2,51 +2,51 @@ export const work = [
   {
     id: 1,
     title: 'YOUNG ENOUGH - "HumanVirus" (Official Music Video)',
-    url: "https://www.youtube.com/watch?v=b-OYZ2c9tiE",
+    url: "https://www.youtube.com/embed/b-OYZ2c9tiE",
   },
   {
     id: 2,
     title: "Eslo x Fazé - Jamais (Clip Officiel)",
-    url: "https://www.youtube.com/watch?v=6TQ9Rr1KS7Q",
+    url: "https://www.youtube.com/embed/6TQ9Rr1KS7Q",
   },
   {
     id: 3,
     title: 'YOUNG ENOUGH - "I Want My K7s Back" (Official Music Video)',
-    url: "https://www.youtube.com/watch?v=HI8Jphj-bsI",
+    url: "https://www.youtube.com/embed/HI8Jphj-bsI",
   },
   {
     id: 4,
     title: "The Lucky Trolls - City Of Iron (Official Music Video)",
-    url: "https://www.youtube.com/watch?v=lH6zRgVbZqU",
+    url: "https://www.youtube.com/embed/lH6zRgVbZqU",
   },
   {
     id: 5,
     title: 'YOUNG ENOUGH - "Minimum Wage" (Official Music Video)',
-    url: "https://www.youtube.com/watch?v=MSmJXV4GCWI",
+    url: "https://www.youtube.com/embed/MSmJXV4GCWI",
   },
   {
     id: 6,
     title: "MR.FARTFACE - LET IT SKATE (OFFICIAL VIDEO)",
-    url: "https://www.youtube.com/watch?v=Uvkzl5baLIg",
+    url: "https://www.youtube.com//embed/Uvkzl5baLIg",
   },
   {
     id: 7,
     title: "Pool Party Chicks - Leave Out Our Best Days",
-    url: "https://www.youtube.com/watch?v=qDVmycxEU_4",
+    url: "https://www.youtube.com//embed/qDVmycxEU_4",
   },
   {
     id: 8,
     title: "Ocean Encounters - Fanatical",
-    url: "https://www.youtube.com/watch?v=6rszXiooIn8",
+    url: "https://www.youtube.com//embed/6rszXiooIn8",
   },
   {
     id: 9,
     title: "Ocean Encounters - Potential",
-    url: "https://www.youtube.com/watch?v=zVPKwGxeM6g",
+    url: "https://www.youtube.com//embed/zVPKwGxeM6g",
   },
   {
     id: 10,
     title: "Get Rid Of Myself (official video)",
-    url: "https://www.youtube.com/watch?v=Y4CVCUxFpjQ",
+    url: "https://www.youtube.com//embed/Y4CVCUxFpjQ",
   },
 ];
