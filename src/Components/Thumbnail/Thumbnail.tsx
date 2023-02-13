caff2388958e4d6c843c3747219c20946b03c6d1
Tim <Tim280696@hotmail.com>
2023-02-13 15:29:47 +0100

    add background image

--- src/Components/Thumbnail/Thumbnail.tsx
+++ src/Components/Thumbnail/Thumbnail.tsx
@@ -4,11 +4,13 @@ import { Link } from "react-router-dom";
 type Props = {
   title: string;
   url: string;
+  background: string;
 };
 
-export default function Thumbnail({ title, url }: Props) {
+export default function Thumbnail({ title, url, background }: Props) {
   return (
     <Link className="thumbnail" to={url}>
+      <img src={background} alt={title} />
       <h2 className="thumbnail-title">{title}</h2>
     </Link>
   );
