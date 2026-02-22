9410ef1899bb308fda347834990bd80094944013
Tim Jeanmart <Tim280696@hotmail.com>
2026-02-22 17:50:49 +0100

    Update VideoFilter.tsx

--- src/Components/VideoFilter/VideoFilter.tsx
+++ src/Components/VideoFilter/VideoFilter.tsx
@@ -10,7 +10,7 @@ interface VideoFilterProps {
 const formatCategoryLabel = (category: string): string => {
   return category
     .split("-")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => word)
     .join(" ");
 };
 
